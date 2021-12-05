d5c54674838b99664b9b41c5ed371f6defb1302b
Eero Ruohola <eero@ruohola.dev>
2021-12-05 18:35:04 +0200

    Add initial landing page infra and a CI/CD pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.40.0"
+    }
+  }
+
+  backend "s3" {
+    // The state bucket has been created manually outside of terraform.
+    bucket  = "apilytics-terraform-state"
+    key     = "terraform.tfstate"
+    encrypt = true
+  }
+}
+
+// The IAM user that the repo is using has been created manually outside of Terraform.
+
+module "landing_page_prod" {
+  source = "./modules/landing-page"
+
+  name = "apilytics-prod-landing-page"
+
+  vpc_cidr_block = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+
+  postgres_dbname   = var.landing_page_prod_postgres_dbname
+  postgres_username = var.landing_page_prod_postgres_username
+  postgres_password = var.landing_page_prod_postgres_password
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "landing_page_prod_postgres_dbname" {
+  type      = string
+  sensitive = true
+}
+
+variable "landing_page_prod_postgres_username" {
+  type      = string
+  sensitive = true
+}
+
+variable "landing_page_prod_postgres_password" {
+  type      = string
+  sensitive = true
+}
