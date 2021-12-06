ed7f7696f48b6a007ad39b2367ff01287e45e8e0
Eero Ruohola <eero@ruohola.dev>
2021-12-06 13:36:26 +0200

    Fix "Error creating DB Instance: Invalid master user name" on deployment

--- terraform/modules/landing-page/rds.tf
+++ terraform/modules/landing-page/rds.tf
@@ -6,8 +6,8 @@ resource "aws_db_instance" "this" {
   instance_class    = "db.t2.micro"
   allocated_storage = 20
   storage_type      = "gp2"
-  username          = var.postgres_password
-  password          = var.postgres_username
+  username          = var.postgres_username
+  password          = var.postgres_password
 
   db_subnet_group_name   = aws_db_subnet_group.this.name
   vpc_security_group_ids = [aws_security_group.this.id]
