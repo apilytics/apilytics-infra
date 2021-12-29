2898fa037b864ad2689949ee8b9445e0b18108b0
Eero Ruohola <eero@ruohola.dev>
2021-12-29 22:22:10 +0200

    Remove SNS email alerts for SES bounces and complaints
    They are not needed since "email feedback forwarding" is already enabled
    by default for the SES domain and it accomplishes exactly the same
    thing.
    
    More info:
    https://docs.aws.amazon.com/ses/latest/DeveloperGuide/monitor-sending-activity-using-notifications-email.html

--- terraform/sns.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_sns_topic" "ses_bounces" {
-  name = "${local.name}-ses-bounces"
-}
-
-resource "aws_sns_topic_subscription" "ses_bounces" {
-  endpoint  = local.alert_email
-  protocol  = "email"
-  topic_arn = aws_sns_topic.ses_bounces.arn
-}
