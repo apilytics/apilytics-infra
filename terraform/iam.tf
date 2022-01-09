bc9b0a5aecd6a5e3fb7d7c17a1802f62f275d990
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-01-10 00:55:14 +0200

    Allow sending non-raw email in SES

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -15,7 +15,7 @@ resource "aws_iam_user_policy_attachment" "send_ses" {
 data "aws_iam_policy_document" "send_ses" {
   statement {
     effect    = "Allow"
-    actions   = ["ses:SendRawEmail"]
+    actions   = ["ses:SendRawEmail", "ses:SendEmail"]
     resources = [aws_ses_domain_identity.apilytics_io.arn]
   }
 }
