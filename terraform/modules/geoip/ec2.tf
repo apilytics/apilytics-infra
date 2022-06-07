b45f5a8c54ba2a1e52f6e931c3882c7774f8f6eb
Markus Blomqvist <blomqvist_markus@hotmail.com>
2022-06-07 19:05:40 +0300

    Add email report cron job
    The job calls the email report API
    with the internal API key as part of
    the EC2 user data.

--- terraform/modules/geoip/ec2.tf
+++ terraform/modules/geoip/ec2.tf
@@ -2,8 +2,18 @@ resource "aws_launch_template" "this" {
   name                   = "${var.name}-lt"
   image_id               = data.aws_ssm_parameter.ecs_ami_id.value
   instance_type          = local.instance_type
-  user_data              = base64encode("#!/bin/bash\necho ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config")
   vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(
+    <<EOF
+      #!/bin/bash
+      echo ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config
+      sudo service cron start
+      echo "* 0-0 * * MON curl --request POST \
+        --url 'https://www.apilytics.io/api/origins/email-reports' \
+        --header 'X-API-Key: ${var.internal_api_key}'" | crontab
+    EOF
+  )
 
   iam_instance_profile {
     arn = aws_iam_instance_profile.ecs_instance.arn
